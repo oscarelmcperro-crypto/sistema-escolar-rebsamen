2a6b62f6a2132aaf9181740040a20599a94b7c9a
oscarelmcperro-crypto <oscarelmcperro@gmail.com>
2026-08-11 03:11:39 -0600

    Update server.js

--- server.js
+++ server.js
@@ -3296,4 +3296,6 @@ app.get('/logout', (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, '0.0.0.0', () => console.log(`🚀 Sistema escolar corriendo en http://localhost:${PORT}`));
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
