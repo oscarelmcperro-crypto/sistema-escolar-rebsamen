6b071925b7d8f08c26295827f734ec2d5f789210
oscarelmcperro-crypto <oscarelmcperro@gmail.com>
2026-08-13 04:48:58 -0500

    otra vez

--- server.js
+++ server.js
@@ -58,13 +58,13 @@ const transportador = nodemailer.createTransport({
     }
 });
 
-// Configuración del Pool Conector de PostgreSQL
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL || `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT || 5432}/${process.env.DB_NAME}`,
+    connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false
     }
 });
+
 
 // Inicialización Segura: Asegurar cuenta Administrador Maestra al arrancar
 (async () => {
